fb99435cf6a02bf38ca8efd8afaab28a9ec2f99c
Muhammad Umer <mumer1297@gmail.com>
2020-07-07 14:08:47 +0500

    import css and social nav component

--- src/components/MainGrid.js
+++ src/components/MainGrid.js
@@ -1,11 +1,14 @@
 import React from "react";
-
+import "../App.css";
 import { Grid } from "@material-ui/core";
+import SocialNav from "./SocialNav";
 
 export const MainGrid = () => {
   return (
     <Grid container spacing={3}>
-      <Grid item xs={12} sm={2}></Grid>
+      <Grid item xs={12} sm={2} className="leftGrid">
+        <SocialNav />
+      </Grid>
       <Grid item xs={12} sm={10}></Grid>
     </Grid>
   );
