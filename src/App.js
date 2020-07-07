6572f2afd2e76c2b17e7b745367d439566ae79f6
Muhammad Umer <mumer1297@gmail.com>
2020-07-07 05:49:30 +0500

    importing MainGrid

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import React from "react";
 import "./App.css";
+import { MainGrid } from "./components/MainGrid";
 
 function App() {
-  return <div className="body"></div>;
+  return (
+    <div className="body">
+      <MainGrid />
+    </div>
+  );
 }
 
 export default App;
