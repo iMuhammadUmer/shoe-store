ab29e19ec6761e8d0b9caf0129a6f73e3fe4ac26
Muhammad Umer <mumer1297@gmail.com>
2020-07-08 12:04:55 +0500

    import component Content.js

--- src/components/MainGrid.js
+++ src/components/MainGrid.js
@@ -2,6 +2,8 @@ import React from "react";
 import "../App.css";
 import { Grid } from "@material-ui/core";
 import SocialNav from "./SocialNav";
+import { Navbar } from "./Navbar";
+import Content from "./Content";
 
 export const MainGrid = () => {
   return (
@@ -9,7 +11,10 @@ export const MainGrid = () => {
       <Grid item xs={12} sm={2} className="leftGrid">
         <SocialNav />
       </Grid>
-      <Grid item xs={12} sm={10}></Grid>
+      <Grid item xs={12} sm={10} className="rightGrid">
+        <Navbar />
+        <Content />
+      </Grid>
     </Grid>
   );
 };
