0e7e8496cc5fb8b55ca800fb73f0cb65bbd67b7a
Muhammad Umer <mumer1297@gmail.com>
2020-07-08 12:05:26 +0500

    [WIP] nav items

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,17 @@
 import React from "react";
+import "../App.css";
 
 export const Navbar = () => {
   return (
     <div>
-      <h1>Navbar</h1>
+      <nav>
+        <ul>
+          <li>Home</li>
+          <li>Collections</li>
+          <li>Contact Us</li>
+          <li>About Us</li>
+        </ul>
+      </nav>
     </div>
   );
 };
