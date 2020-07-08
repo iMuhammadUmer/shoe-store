94a586e88e37399c5228b44d20be81c3092c13fa
Muhammad Umer <mumer1297@gmail.com>
2020-07-08 12:33:29 +0500

    import content

---
+++ src/components/Home.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { MainGrid } from "./MainGrid";
+import Content from "./Content";
+
+export const Home = () => {
+  return (
+    <div>
+      <MainGrid />
+      <Content>
+        <h1>Work in progress</h1>
+      </Content>
+    </div>
+  );
+};
