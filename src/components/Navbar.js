4807b333509b824ac6e024a6c1355456eed5242e
Muhammad Umer <mumer1297@gmail.com>
2020-07-11 07:05:27 +0500

    update navbar with Link

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,15 +1,16 @@
 import React from "react";
 import "../App.css";
+import { Link } from "react-router-dom";
 
 export const Navbar = () => {
   return (
     <div>
       <nav>
         <ul>
-          <li>Home</li>
-          <li>Collections</li>
-          <li>Contact Us</li>
-          <li>About Us</li>
+          <Link to="/">Home</Link>
+          <Link to="/">Collection</Link>
+          <Link to="/">Contact Us</Link>
+          <Link to="/">About Us</Link>
         </ul>
       </nav>
     </div>
