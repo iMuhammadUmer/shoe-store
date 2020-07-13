24f5bc2a1cd7a94deefab5f97b8cf22e3a0b60eb
Muhammad Umer <mumer1297@gmail.com>
2020-07-13 19:08:26 +0500

    updating navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,16 +1,19 @@
 import React from "react";
 import "../App.css";
 import { Link } from "react-router-dom";
+import logo from "../images/logo.svg";
 
 export const Navbar = () => {
   return (
     <div>
       <nav>
         <ul>
+          <Link to="/" className={"link"}>
+            <img src={logo} alt="logo" />
+          </Link>
           <Link to="/">Home</Link>
-          <Link to="/">Collection</Link>
-          <Link to="/">Contact Us</Link>
-          <Link to="/">About Us</Link>
+          <Link to="#">Collection</Link>
+          <Link to="/about">About Us</Link>
         </ul>
       </nav>
     </div>
