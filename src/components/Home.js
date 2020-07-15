37d8def9070ecc26b888fe9c5afc55a0173f8e4b
Muhammad Umer <mumer1297@gmail.com>
2020-07-15 06:52:32 +0500

    add grid

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,35 @@
 import React from "react";
+import "../App.css";
+import { makeStyles } from "@material-ui/core/styles";
+import Paper from "@material-ui/core/Paper";
+import Grid from "@material-ui/core/Grid";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+    display: "flex",
+    justifyContent: "center",
+    backgroundColor: "blue",
+  },
+  paper: {
+    padding: theme.spacing(2),
+    textAlign: "center",
+    color: theme.palette.text.secondary,
+  },
+}));
 
 export const Home = () => {
+  const classes = useStyles();
+
   return (
-    <div>
-      <h1>Welcome to Home</h1>
+    <div className={classes.root}>
+      <Grid container>
+        <Grid item xs={12} sm={12}>
+          <Paper className={classes.paper}>
+            <h1>Welcome to Home</h1>
+          </Paper>
+        </Grid>
+      </Grid>
     </div>
   );
 };
