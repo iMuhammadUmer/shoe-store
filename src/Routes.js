38fe508a55119b1f2c4dc2bdae58c4be1a07387e
Muhammad Umer <mumer1297@gmail.com>
2020-07-19 10:44:49 +0500

    routes updated

--- src/Routes.js
+++ src/Routes.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Home } from "./components/Home";
 import { About } from "./components/About";
-import { Collection } from "./components/Collection.js";
+import { Products } from "./components/Products.js";
 import { Navbar } from "./components/Navbar";
 
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
@@ -13,7 +13,7 @@ function RouteConfig() {
         <Navbar />
         <Switch>
           <Route exact path="/" component={Home} />
-          <Route exact path="/collection" component={Collection} />
+          <Route exact path="/products" component={Products} />
           <Route path="/about" component={About} />
           <Route path="*" component={() => <h2>Error 404</h2>} />
         </Switch>
